f2ac3d61481224530aedbaab553c9e293f31cde1
azertej <azer.tej.2001@gmail.com>
2024-08-28 16:37:23 +0100

    fix vercel cache problem

--- app/api/aboutPage/get-aboutPage/route.ts
+++ app/api/aboutPage/get-aboutPage/route.ts
@@ -1,11 +1,19 @@
 import { connectToDB } from "@/utils/database"
 import { aboutPageModel } from "@/models/aboutPageModel"
 
-export const GET = async (req:any) => {
+export const GET = async (req: any) => {
     try {
         await connectToDB()
         const infos = await aboutPageModel.find()
-        return new Response(JSON.stringify(infos), { status: 200 })
+        return new Response(JSON.stringify(infos), {
+            status: 200,
+            headers: {
+                "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+                "Pragma": "no-cache",
+                "Expires": "0",
+                "Content-Type": "application/json",
+            },
+        })
     } catch (error) {
         return new Response(`${error}`, { status: 500 })
     }
